23065f784e7c9fc2c473f2ee4236193aa6df4c41
Marcus Vinicius <tamodoidogamer@gmail.com>
2026-02-05 15:07:50 -0300

    Update entradaesaidadedados.c

--- entrada e saida de dados/exercicio03/entradaesaidadedados.c
+++ entrada e saida de dados/exercicio03/entradaesaidadedados.c
@@ -2,10 +2,21 @@
 
 int main() {
     int idade;
+    float altura;
+    char nome[30];
 
     printf("Digite sua idade: ");
     scanf("%d", &idade);
 
     printf("Sua idade: %d\n", idade);
 
+    printf("Digite a sua altura: ");
+    scanf("%f", &altura);
+    printf("Sua altura: %.2f\n", altura);
+
+    printf("Digite seu nome: ");
+    scanf("%s", &nome);
+    printf("Nome: %s\n", nome);
+
+    return 0;
 }
