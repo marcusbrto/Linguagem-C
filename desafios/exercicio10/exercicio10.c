4786a38155b1a1293a6233f5eebe5752ec2af5d5
Marcus Vinicius <tamodoidogamer@gmail.com>
2026-02-07 16:05:41 -0300

    uptade

---
+++ desafios/exercicio10/exercicio10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int capestadio,qntpessoas,taxa;
+    printf("Qual a capacidade total do estadio? ");
+    scanf("%d",&capestadio);
+    printf("Qual a quantidade de torcedores presente no jogo? ");
+    scanf("%d",&qntpessoas);
+    taxa = ((float)qntpessoas/capestadio)*100;
+    if (taxa > 90) {
+        printf("Lotado!");
+    }else if (taxa > 70 && taxa <= 90) {
+        printf("Otima presenca de publico!");
+    }else if (taxa >= 50 && taxa <= 70) {
+        printf("Publico razoavel!");
+    }else {
+        printf("Morumbis!");
+    }
+
+}
