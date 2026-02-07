a5e94aad6dda8efb81bc17877c6e5347b88b6ad4
Marcus Vinicius <tamodoidogamer@gmail.com>
2026-02-07 10:59:11 -0300

    new exercise

---
+++ desafios/exercicio04/exercicio04.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int vitorias,empates,derrotas,total;
+    printf("Digite a quantidade de VITORIAS:");
+    scanf("%d",&vitorias);
+    printf("Digite a quantidade de empates:");
+    scanf("%d",&empates);
+    printf("Digite a quantidade de derrotas:");
+    scanf("%d",&derrotas);
+    vitorias = vitorias * 3;
+    empates = empates * 1;
+    total = empates + vitorias;
+    printf("=========================\n");
+    printf("A quantidade total de pontos foi de %d \n",total);
+}
