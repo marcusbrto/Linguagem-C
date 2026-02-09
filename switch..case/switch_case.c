9cbd2eed8dcb3f7483a75d8a53fe9b3fcf44d112
Marcus Vinicius <tamodoidogamer@gmail.com>
2026-02-09 16:26:33 -0300

    exercise

---
+++ switch..case/switch_case.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    switch(opcao){
+        case 1:
+        printf("Opcao 1\n");
+        break;
+        case 2:
+        printf("Opcao 2\n");
+        break;
+        case 3:
+        printf("Opcao 3\n");
+        break;
+        case 4:
+        printf("Opcao 4\n");
+        break;
+        default:
+        printf("Opcao INVALIDA\n");
+    }
+
+    return 0;
+}
