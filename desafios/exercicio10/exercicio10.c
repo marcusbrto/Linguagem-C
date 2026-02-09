0056660e7b3315d58cffffb55ca2f6d80931f461
Marcus Vinicius <tamodoidogamer@gmail.com>
2026-02-09 16:01:24 -0300

    Update exercicio10.c

--- desafios/exercicio10/exercicio10.c
+++ desafios/exercicio10/exercicio10.c
@@ -9,12 +9,13 @@ int main() {
     taxa = ((float)qntpessoas/capestadio)*100;
     if (taxa > 90) {
         printf("Lotado!");
-    }else if (taxa > 70 && taxa <= 90) {
+    }else if (taxa >= 70) {
         printf("Otima presenca de publico!");
     }else if (taxa >= 50 && taxa <= 70) {
         printf("Publico razoavel!");
     }else {
         printf("Morumbis!");
     }
+    printf("\nLotacao: %d%%",taxa);
 
 }
