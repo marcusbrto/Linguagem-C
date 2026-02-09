7cdc5edd1440892e31b11143c66046b749ab4e60
Marcus Vinicius <tamodoidogamer@gmail.com>
2026-02-09 09:41:29 -0300

    Update exercicio05.c

--- desafios/exercicio05/exercicio05.c
+++ desafios/exercicio05/exercicio05.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <math.h> //usando somente para deixar o valor absoluto sem o sinal
+
 int main() {
     float salario1, salario2, diferenca;
     printf("Digite o salario do primeiro jogador: ");
     scanf("%f", &salario1);
     printf("Digite o salario do segundo jogador: ");
     scanf("%f", &salario2);
-    diferenca = salario1 - salario2;
+    diferenca = fabs(salario1 - salario2);//fabs é float absolute, ai não aparece o sinal
     printf("=====================\n");
     printf("A diferenca de salarios eh de %.2f",diferenca);
 }
