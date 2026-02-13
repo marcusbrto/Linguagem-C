f92aa1e03596b5a551d852f64ad918d2f927271d
Marcus Vinicius <tamodoidogamer@gmail.com>
2026-02-13 14:41:14 -0300

    update

---
+++ loop FOR - Quadrado de asteristico/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Digite um numero para o tamanho do quadrado: ");
+    scanf("%d",&n);
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+
+}
