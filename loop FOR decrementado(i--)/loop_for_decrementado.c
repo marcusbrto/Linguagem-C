3a1593d3b93c8073f90c0be59040bfbf4967a237
Marcus Vinicius <tamodoidogamer@gmail.com>
2026-02-13 13:51:36 -0300

    update

---
+++ loop FOR decrementado(i--)/loop_for_decrementado.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Contagem refressiva para a bomba explodir:");
+    for (int i = 10; i >= 1; i--)
+    {
+        printf("\n %d",i);
+    }
+    printf("\nBOOM!!");
+}
