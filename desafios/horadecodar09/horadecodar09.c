228ec9cd232931534283b0b0abd162f165e09377
Marcus Vinicius <tamodoidogamer@gmail.com>
2026-03-11 14:12:23 -0300

    novos exercicios

---
+++ desafios/horadecodar09/horadecodar09.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+//Funcao recursiva para imprimir numeros de n ate 1
+void recursiveLoop(int n) {
+
+    if (n > 0) {
+       // printf("%d \n", n); // Imprime o valor atual de n
+        recursiveLoop(n - 1); // Chama a si mesma com n = 1
+        printf("%d \n", n); //tem que tomar cuidado com a ordem dos fatores se nao ele muda o produto
+
+    }
+
+}
+
+int main() {
+
+    int num = 10;//Poderia ter sido adicionado o numero direto na chamada mas como o certo é com funcao entao fiz assim
+    printf("Contagem regressiva:\n");
+    recursiveLoop(num);
+    printf("BOOM!");
+
+    return 0;
+}
